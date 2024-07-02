b9cc9c22d33708a141f771b494e0e8aff199753d
CallMeYurii <vad4807@gmail.com>
2024-07-02 14:05:26 +0300

    Add sqllite with auto seeding

--- src/Services/Discount/Discount.Grpc/Program.cs
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -1,16 +1,27 @@
+using Discount.Grpc.Data;
+using Discount.Grpc.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddGrpc();
-builder.Services.AddGrpcReflection();
+// builder.Services.AddGrpcReflection();
+
+builder.Services.AddDbContext<DiscountContext>(opts =>
+{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-    app.MapGrpcReflectionService();
+app.UseMigration();
+
+// if (app.Environment.IsDevelopment())
+//     app.MapGrpcReflectionService();
 
 // Configure the HTTP request pipeline.
-// app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
